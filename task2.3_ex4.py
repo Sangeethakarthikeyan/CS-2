fc6c6af34fe88ae33153fb7cf29ae2caff3c32d4
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-10-17 17:07:01 +1100

    Create task2.3_ex4.py

---
+++ task2.3_ex4.py
@@ -0,0 +1,14 @@
+width=17
+height=12.0
+x=width//2
+xtype=type(x)
+print('The value of x is', x, 'and its type is', xtype)
+y=width/2.0
+ytype=type(y)
+print('The value of y is', y, 'and its type is', ytype)
+z=height/3
+ztype=type(z)
+print('The value of z is', z, 'and its type is', ztype)
+n=1 + 2 * 5
+ntype=type(n)
+print('The value of n is', n, 'and its type is', ntype)
