0d825dc167509eb1e91d64893face8ad2e4e57d1
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-10-17 12:18:05 +1100

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
-input1= raw_input("Enter first number")
-input2=raw_input("Enter second number")
-answer= input1 + input2
-print('The sum is', answer)
+input1= input("Enter first number")
+input2= input("Enter second number")
+answer= float(input1) + float(input2)
+print('The sum of',input1 ,'and' ,input2 ,'is',answer)
