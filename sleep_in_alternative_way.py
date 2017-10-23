ca393761fa6396b24bce9d48cb853b2e5f13856e
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-10-23 12:17:18 +1100

    Create sleep_in_alternative_way.py

---
+++ sleep_in_alternative_way.py
@@ -0,0 +1,9 @@
+# Get input to know if it is a weekday
+weekday=input('Is it a weekday? Enter Y if yes and N if no \n')
+vacation=input('Are we on a vacation? Enter Y if yes and N if no \n')
+
+
+# Decide if sleep in or not
+if (weekday=='Y' or weekday=='N') and (vacation=='Y' or vacation=='N'): print('sleep in : ', weekday=='N' or vacation=='Y' )
+else: 
+	print('Invalid input. Please enter Y or N')
