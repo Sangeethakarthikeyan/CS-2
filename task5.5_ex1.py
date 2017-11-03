cf95be615355ef765fc0ceb0d018adda76d7e49b
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-11-03 11:30:43 +1100

    Update task5.5_ex1.py

--- task5.5_ex1.py
+++ task5.5_ex1.py
@@ -6,14 +6,28 @@ while True:
 	try: 
 		number=input('Enter a number:  ') 	# Get input
 		if(number)=='done': break			#Stop getting input if input is 'done'		
-		number=int(number)					
+		number=int(float(number))					
 		#To update total and count
 		total=total+number
 		count=count+1
+
 	except:
 		print('Invalid input')
+# Compute average (3methods to avoid ZeroDivisionError)
+
+avg=(total/count) if count!=0 else 0
+
+'''try: avg=(total/count)
+except ZeroDivisionError:
+	avg=0'''
 	
-avg=total/count								# Compute average
-print(total,count,avg)						# print the final values of total, count, average
+'''if count==0: avg=0
+else: avg=(total/count)'''
+
+	
+# print the final values of total, count, average												
+print(total,count,avg)			
+
+
 
 
