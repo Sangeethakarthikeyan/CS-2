72cab7b04c10c8a878d204e215f344c4cc9e4b42
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-11-20 17:12:01 +1100

    Update task6.3_ex6.py

--- task6.3_ex6.py
+++ task6.3_ex6.py
@@ -1,4 +1,7 @@
 print('British to American English spelling: replace ise with ize')
+
 str = '   apologise, organise, recognise,summarise,categorise,visualise    '
-print('British English:', str.strip())
-print('American English:', str.strip().replace('ise','ize'))
+
+print('British English:', str.strip()) # print without white spaces 
+
+print('American English:', str.strip().replace('ise','ize'))   # print without white spaces and replace 'ise' with 'ize'
