f30a150ee1555d759704856265767c400ca57b4d
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-11-20 11:33:09 +1100

    Update license_cp2.py

--- license_cp2.py
+++ license_cp2.py
@@ -1,11 +1,9 @@
-import sys
+iimport sys
 
 print('LICENSE ISSUE CHECK')
 
 def license_issue(age,hours):
-	if age<16:
-		if hours<200: print('License not issued.Age should be 16 or more and practice hours should be 200 or more')
-		else: print('License not issued. Age should be 16 or more')
+	if age<16: print('License not issued.Age should be 16 or more')
 	elif hours<200: print('License not issued. Practice hours should be 200 or more')
 	else: print('Congratulations. License issued')
 
@@ -13,9 +11,8 @@ try:
 	age=float(input('Enter your age:  '))
 	hours=float(input('Enter practise hours:  '))
 except:
-	print('Enter numeric input')
+	print('Invalid input.Enter numeric input only')
 	sys.exit()
 
 license_issue(age,hours)
-
 
