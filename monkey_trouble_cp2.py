a71f80a36e54d563a57eae3a74d1508339d162db
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-11-20 11:42:59 +1100

    Update monkey_trouble_cp2.py

--- monkey_trouble_cp2.py
+++ monkey_trouble_cp2.py
@@ -4,12 +4,13 @@ def monkey_trouble(a,b):
 
 # Get inputs to know if monkey-a and b are smiling or not
 a=input('Is monkey-a smiling? Enter Y if yes and N if no \n').upper()
-b=input('Is monkey-b smiling? Enter Y if yes and N if no \n').upper()
-
 # Check if the input is Y or N. If not print invalid input
-	if (a=='Y' or a=='N') and (b=='Y' or b=='N') :
-		monkey_trouble(a,b)	# calling function monkey_trouble	
+if (a=='Y' or a=='N'):
+	b=input('Is monkey-b smiling? Enter Y if yes and N if no \n').upper()
+	if (b=='Y' or b=='N') : monkey_trouble(a,b)	# calling function monkey_trouble	
 	else: print('Invalid input. Please enter Y or N')
+else: print('Invalid input. Please enter Y or N')
+
 
 
 
