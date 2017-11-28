c5c12e8db65e960e366965c3a9cfff28e69de90d
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-11-28 13:15:09 +1100

    Create first_two.py

---
+++ first_two.py
@@ -0,0 +1,8 @@
+#function definition of make_abba
+def firat_two(a):
+	return a[0:2]
+
+# Getting a string as user input
+a=input('Enter a string:')
+print('output: ', firat_two(a))  # Function call in the print statement
+
