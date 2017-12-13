5c2972f269cde745865a3b221eb696f1dd95bb6a
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-12-13 13:04:38 +1100

    Update task8.4_ex5.py

--- task8.4_ex5.py
+++ task8.4_ex5.py
@@ -10,7 +10,7 @@ for line in fhand:
 	words=line.split()
 	#print('Debug: ', words)
 	if len(words)==0: continue			# To avoid traceback due to blanklines
-	if words[0]!='From': continue		# To skip uninterested lines
-	count=count+1						# To keep count of interested lines
-	print (words[1])
+	if words[0]=='From' and len(words)>1:		# To count interested lines and guardian code for line 15						
+		count=count+1
+		print (words[1])
 print("There are {} lines in the file with From as the first word".format(count))
