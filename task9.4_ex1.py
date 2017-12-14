eac72d6fd8e6ef415ec5a30eee29c35a25a5373b
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-12-14 17:14:57 +1100

    Create task9.4_ex1.py

---
+++ task9.4_ex1.py
@@ -0,0 +1,11 @@
+fhandle=open('romeo.txt')
+sample=dict()				# sample is the name of the empty dictionary
+for line in fhandle:
+	line=line.rstrip()		# To avoid \n at the end of each line
+	words=line.split()
+	for word in words:
+		sample[word]=sample.get(word,0)+1		# add the word in dictionary if it is first occurence, else add one to value each time it occurs
+print(sample)
+
+check=input('\n \n Enter a word to be checked if in dictionary or not: ')  
+print(check in sample)
