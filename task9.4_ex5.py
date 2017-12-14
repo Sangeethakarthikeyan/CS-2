199c4f29a09a2fd053bfd76dc84680127e31523f
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-12-15 10:38:33 +1100

    Update task9.4_ex5.py

--- task9.4_ex5.py
+++ task9.4_ex5.py
@@ -14,7 +14,11 @@ for line in fhandle:
 	words=line.split()
 	if len(words)==0 :continue		# To skip empty lines (Guardian code)
 	if words[0]=='From' and len(words)>2:	# To go to interested lines and guardian code for words[1]
-		email=words[1]
-		domain=email.split('@')				# double split to get domain seperately
-		sample[domain[1]]=sample.get(domain[1],0)+1     # add the word in dictionary if it is first occurence, else add one to value each time it occurs
+		
+		#email=words[1]
+		#domain=email.split('@')
+		#sample[domain[1]]=sample.get(domain[1],0)+1
+
+		domain=words[1].split('@')[1]			# double split to get domain seperately
+		sample[domain]=sample.get(domain,0)+1     # add the word in dictionary if it is first occurence, else add one to value each time it occurs
 print(sample)
