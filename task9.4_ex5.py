e100de2e33d253f542130c2ffd548f6c350cda89
Sangeethakarthikeyan <32557874+Sangeethakarthikeyan@users.noreply.github.com>
2017-12-14 19:16:17 +1100

    Create task9.4_ex5.py

---
+++ task9.4_ex5.py
@@ -0,0 +1,20 @@
+import sys
+
+fname=input('Enter a file name: ')
+
+try:
+	fhandle=open(fname)
+except:
+	print('File cannot be opened', fname)
+	sys.exit()
+
+sample=dict()				# sample is the name of the empty dictionary
+for line in fhandle:
+	line=line.rstrip()		# To avoid \n at the end of each line
+	words=line.split()
+	if len(words)==0 :continue		# To skip empty lines (Guardian code)
+	if words[0]=='From' and len(words)>2:	# To go to interested lines and guardian code for words[1]
+		email=words[1]
+		domain=email.split('@')				# double split to get domain seperately
+		sample[domain[1]]=sample.get(domain[1],0)+1     # add the word in dictionary if it is first occurence, else add one to value each time it occurs
+print(sample)
